5e866b7e70df11318f1b319845a037936e0142a5
Max Sabirov <maxim-sabirov@rambler.ru>
2021-06-15 19:18:52 +0300

    New In Game UI

---
+++ NewProject/Assets/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject shopPanel;
+    public GameObject buttonsPanel;
+    private bool _isInShop;
+
+    private void Start()
+    {
+        _isInShop = false;
+    }
+
+    public void ShopInGame()
+    {
+        _isInShop = !_isInShop;
+        shopPanel.SetActive(_isInShop);
+        buttonsPanel.SetActive(!_isInShop);
+    }
+}
