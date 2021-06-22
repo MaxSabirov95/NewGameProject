e11239af9cc558aed4ecae1a6cc62c29e28564a7
Max Sabirov <maxim-sabirov@rambler.ru>
2021-06-22 17:18:38 +0300

    UI upgrade Fix

---
+++ NewProject/Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverManager : MonoBehaviour
+{
+    [SerializeField] GameObject _buttonsPanel;
+    [SerializeField] GameObject _upgradePanel;
+    private bool _isUpgradePanelOn = false;
+
+    public void UpgradePanel()
+    {
+        _isUpgradePanelOn = !_isUpgradePanelOn;
+        _buttonsPanel.SetActive(!_isUpgradePanelOn);
+        _upgradePanel.SetActive(_isUpgradePanelOn);
+    }
+}
